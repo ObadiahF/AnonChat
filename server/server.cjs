975f4d18a58282c89b9cbfbb19af497dd1e863af
Obadiah F <106855190+ObadiahF@users.noreply.github.com>
2023-06-21 00:25:06 -0700

    Added server functionality for creating rooms and joining them. I have not yet connected the server to the front-end

--- server/server.cjs
+++ server/server.cjs
@@ -20,6 +20,93 @@ const io = require("socket.io")(server, {
   }
 });
 
+const rooms = [
+  {
+    "roomName": "GLOBAL CHAT",
+    "roomPassword": "",
+    "users": []
+  }
+];
+
+app.get('/', (req, res) => {
+  res.status(200).send('test');
+})
+
+app.post('/createRoom', (req, res) => {
+  try {
+    const { body } = req;
+    let { roomName, roomPassword } = body;
+
+    if (!roomName) {
+      res.status(406).send('No room name provided')
+    }
+    
+    rooms.forEach(room => {
+      if (roomName === room.roomName) {
+        res.status(403).send('Room already exists');
+        return;
+      }
+    })
+    
+    if (!roomPassword) {
+      console.log('no')
+      roomPassword = "";
+    }
+
+
+
+    rooms.push(
+      {
+        roomName,
+        roomPassword,
+        "users": []
+      }
+    )
+
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(400);
+  }
+
+});
+
+
+app.get('/checkGamesExistance', (req, res) => {
+  try {
+    const { body } = req;
+    let { roomName, roomPassword } = body;
+
+    if (!roomName) {
+      res.status(406).send('No Room Name Provided')
+    }
+    if (!roomPassword) {
+      roomPassword = "";
+    }
+
+
+    rooms.forEach((room) => {
+      if (roomName === room.roomName) {
+        if (room.roomPassword === "" || roomPassword === room.roomPassword) {
+          res.sendStatus(200);
+        } else {
+          res.status(403).send("Incorrect Password")
+        }
+      }
+    })
+    res.status(404).send('Room not found');
+
+  } catch {
+    res.status(400);
+  }
+})
+
+io.on('connection', (client) => {
+
+
+})
+
+
+
 server.listen(3000, () => {
     console.log('websocket port: 3000');
   })
